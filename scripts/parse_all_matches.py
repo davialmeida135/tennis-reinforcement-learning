1d8569da70d57254678abb143769ea4a9f0b3cf9
Davi Almeida <davi.almeida.135@ufrn.edu.br>
2025-11-16 17:00:55 -0300

    scripts/parse all matches

---
+++ scripts/parse_all_matches.py
@@ -0,0 +1,20 @@
+from app.data.match_parser import parse_all_points
+import polars as pl
+import pathlib
+
+if __name__ == "__main__":
+    files = [
+    #    "charting-m-points-to-2009.csv",
+    #    "charting-m-points-2010s.csv",
+        "charting-m-points-2020s.csv"
+    ]
+    for file_name in files:
+        project_root = pathlib.Path(__file__).parent.parent
+        data_path = project_root / "data" / "raw" / file_name
+        data_path.parent.mkdir(parents=True, exist_ok=True)
+        target_path = project_root / "data" / "processed" / f"parsed_{file_name}"
+        target_path.parent.mkdir(parents=True, exist_ok=True)
+        df = pl.read_csv(data_path)
+        print(f"Parsing file: {file_name}")
+        parsed_all = parse_all_points(df)
+        parsed_all.to_pandas().to_csv(target_path, index=False)
