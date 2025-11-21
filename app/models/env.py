8451a78dba5354505bcede7ff9b553003db3d99b
Davi Almeida <davi.almeida.135@ufrn.edu.br>
2025-11-21 18:03:09 -0300

    initial env

---
+++ app/models/env.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class Action(BaseModel):
+    shot_type: str
+    shot_direction: str
+
+
+class State(BaseModel):
+    last_shot_type: str
+    last_shot_direction: str
+    player_game_score: str
+    player_set_score: int
+    pc_game_score: str
+    pc_set_score: int
+    player_serves: bool
+    
+    def to_tuple(self) -> tuple:
+        return (
+            self.last_shot_type,
+            self.last_shot_direction,
+            self.player_game_score,
+            self.player_set_score,
+            self.pc_game_score,
+            self.pc_set_score,
+            self.player_serves
+        )
